3aa7568dbe0d859a856b40897a6e5a6707fb14c7
CharlosFerdinand <74392017+CharlosFerdinand@users.noreply.github.com>
2024-05-18 20:50:59 +0700

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,23 +1,61 @@
+import React, { useState } from "react";
 import People from "./People";
-import Person from "./Person";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "./AboutUs.css";
+
 const About = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const person = People[currentIndex];
+
+  const handleNext = () => {
+    setCurrentIndex((currentIndex + 1) % People.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((currentIndex - 1 + People.length) % People.length);
+  };
+
   return (
-    <div className="background">
-      <img src={require("./IMG/Pempek/campuranPempek.jpg")} className="bgIMG" />
+    <div>
+      <img
+        src={require("./IMG/Pempek/campuranPempek.jpg")}
+        className="bgDwIMG"
+        alt="background"
+      />
       <div className="headFiller"></div>
       <div className="daWrap">
         <Container>
-          <Row>
-            {People.map((person, index) => (
-              <Col key={index} sm={6}>
-                <Person
-                  name={person.name}
-                  image={person.image}
-                  info={person.info}
-                />
-              </Col>
-            ))}
+          <Row className="align-items-center justify-content-center text-center text-md-start">
+            <Col
+              xs={12}
+              md={4}
+              className="d-flex justify-content-center mb-3 mb-md-0"
+            >
+              <img className="picture1" src={person.image} alt={person.name} />
+            </Col>
+            <Col xs={12} md={8} className="text-container">
+              <h2>{person.name}</h2>
+              <p>{person.info}</p>
+              <p>{person.info2}</p>
+            </Col>
+          </Row>
+          <Row className="justify-content-center mt-3">
+            <Col xs={6} md={3} className="d-flex justify-content-between">
+              <Button
+                variant="primary"
+                onClick={handlePrev}
+                className="w-100 me-2"
+              >
+                Previous
+              </Button>
+              <Button
+                variant="primary"
+                onClick={handleNext}
+                className="w-100 ms-2"
+              >
+                Next
+              </Button>
+            </Col>
           </Row>
         </Container>
       </div>
