68ec0f36723e9e3b4cc31566af8845a815666077
CharlosFerdinand <74392017+CharlosFerdinand@users.noreply.github.com>
2024-05-18 20:51:27 +0700

    Update People.js

--- src/People.js
+++ src/People.js
@@ -1,23 +1,27 @@
 const People = [
   {
-    name: "Charlos Ferdinand",
+    name: "Charlos Ferdinand - Leader",
     image: require("./IMG/Person1.jpg"),
-    info: "Leader",
+    info: "Lakad Matatag! Normalin Normalin",
+    info2: "IG : @charlos_ferdinand",
   },
   {
-    name: "Daniel Wang",
+    name: "Daniel Wang - API Man",
     image: require("./IMG/Person7.jpg"),
-    info: "API Man",
+    info: "Life drags you around. You don't have a say in it",
+    info2: "IG : @danilw3892",
   },
   {
-    name: "Nathan A. Pranata",
+    name: "Nathan A. Pranata - Router Master",
     image: require("./IMG/Person4.jpg"),
-    info: "Router Master",
+    info: "Achieve your goal one step at a time",
+    info2: "IG : @nalaire_",
   },
   {
-    name: "Eric Wang",
+    name: "Eric Wang - Recipe Man",
     image: require("./IMG/Person5.jpg"),
-    info: "Recipe Man",
+    info: "Let the sky be your goal and not let the goal be the end of it all",
+    info2: "IG : @ericcc_wanggg",
   },
 ];
 
